bc953ff09346288c2fd48bbaf9f0b2a949f48ff3
Matthew Macdonald-Wallace <matt@doics.co>
2024-01-07 20:05:23 +0000

    SDS011 still not reading correctly

--- src/bme.cpp
+++ src/bme.cpp
@@ -7,6 +7,7 @@
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BME680.h"
 #include <TinyGPS++.h>
+#include <esp_sds011.h>
 
 
 
@@ -46,8 +47,8 @@ static uint32_t appTxDutyCycle = 60000;
 
 // Sensor values
 
-float pm25 = 0.0;
-float pm10 = 0.0;
+int ext_pm25 = 0;
+int ext_pm10 = 0;
 float temperature = 0.0;
 float humidity = 0.0;
 float airpressure = 0.0;
@@ -89,8 +90,8 @@ static void smartDelay(unsigned long ms)
   unsigned long start = millis();
   do
   {
-    while (Serial1.available())
-      gps.encode(Serial1.read());
+    while (Serial2.available())
+      gps.encode(Serial2.read());
   } while (millis() - start < ms);
 }
 
@@ -111,7 +112,7 @@ static void printFloat(float val, bool valid, int len, int prec)
     for (int i=flen; i<len; ++i)
       Serial.print(' ');
   }
-  smartDelay(0);
+  smartDelay(20);
 }
 
 static void printInt(unsigned long val, bool valid, int len)
@@ -125,7 +126,7 @@ static void printInt(unsigned long val, bool valid, int len)
   if (len > 0)
     sz[len-1] = ' ';
   Serial.print(sz);
-  smartDelay(0);
+  smartDelay(20);
 }
 
 static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
@@ -153,7 +154,7 @@ static void printDateTime(TinyGPSDate &d, TinyGPSTime &t)
   }
 
   printInt(d.age(), d.isValid(), 5);
-  smartDelay(0);
+  smartDelay(20);
 }
 
 static void printStr(const char *str, int len)
@@ -161,7 +162,7 @@ static void printStr(const char *str, int len)
   int slen = strlen(str);
   for (int i=0; i<len; ++i)
     Serial.print(i<slen ? str[i] : ' ');
-  smartDelay(0);
+  smartDelay(20);
 }
 
 static void wakeUp()
@@ -246,8 +247,8 @@ static void prepareTxFrame( uint8_t port )
   lpp->addTemperature(0, temperature);
   lpp->addBarometricPressure(0, airpressure);
   lpp->addRelativeHumidity(0, humidity);
-  lpp->addAnalogInput( 1, pm25 );
-  lpp->addAnalogInput( 2, pm10);
+  lpp->addAnalogInput( 1, ext_pm25 );
+  lpp->addAnalogInput( 2, ext_pm10);
   lpp->addAnalogInput( 3, gas);
   lpp->addGPS(0, gps.location.lat(), gps.location.lng(), gps.altitude.meters());
 
@@ -257,12 +258,96 @@ static void prepareTxFrame( uint8_t port )
 }
 /******************************* Air Quality Sensor **********************************************/
 
-#define SDSTX 38              //pin number for TX output from ESP32 - RX into GPS
-#define SDSRX 39              //pin number for RX input into ESP32 - TX from GPS
+#define SDSTX 12              //pin number for TX output from ESP32 - RX into SDS011
+#define SDSRX 13              //pin number for RX input into ESP32 - TX from SDS011
 
-#define SDSserial Serial1     //define GPSserial as ESP32 Serial2 
+//#define SDSserial Serial1     //define SDSserial as ESP32 Serial1 
+HardwareSerial& SDSserial(Serial1);
+Sds011Async< HardwareSerial > sds011(SDSserial);
+
+// The example stops the sensor for 210s, then runs it for 30s, then repeats.
+// At tablesizes 20 and below, the tables get filled during duty cycle
+// and then measurement completes.
+// At tablesizes above 20, the tables do not get completely filled
+// during the 30s total runtime, and the rampup / 4 timeout trips,
+// thus completing the measurement at whatever number of data points
+// were recorded in the tables.
+constexpr int pm_tablesize = 20;
+int pm25_table[pm_tablesize];
+int pm10_table[pm_tablesize];
+                              //
+bool is_SDS_running = true;
+
+void start_SDS() {
+    Serial.println(F("Start wakeup SDS011"));
+
+    if (sds011.set_sleep(false)) { is_SDS_running = true; }
+
+    Serial.println(F("End wakeup SDS011"));
+}
+
+void stop_SDS() {
+    Serial.println(F("Start sleep SDS011"));
+
+    if (sds011.set_sleep(true)) { is_SDS_running = false; }
+
+    Serial.println(F("End sleep SDS011"));
+}
 
 void run_aq_sensor() {
+        // Per manufacturer specification, place the sensor in standby to prolong service life.
+    // At an user-determined interval (here 210s down plus 30s duty = 4m), run the sensor for 30s.
+    // Quick response time is given as 10s by the manufacturer, thus the library drops the
+    // measurements obtained during the first 10s of each run.
+
+    constexpr uint32_t down_s = 210;
+
+    stop_SDS();
+    Serial.print(F("stopped SDS011 (is running = "));
+    Serial.print(is_SDS_running);
+    Serial.println(')');
+
+    uint32_t deadline = millis() + down_s * 1000;
+    while (static_cast<int32_t>(deadline - millis()) > 0) {
+        delay(1000);
+        Serial.println(static_cast<int32_t>(deadline - millis()) / 1000);
+        sds011.perform_work();
+    }
+
+    constexpr uint32_t duty_s = 30;
+
+    start_SDS();
+    Serial.print(F("started SDS011 (is running = "));
+    Serial.print(is_SDS_running);
+    Serial.println(')');
+
+    sds011.on_query_data_auto_completed([](int n) {
+        Serial.println(F("Begin Handling SDS011 query data"));
+        Serial.print(F("n = ")); Serial.println(n);
+        int pm25 = 0;
+        int pm10 = 0;
+        if (sds011.filter_data(n, pm25_table, pm10_table, pm25, pm10) &&
+            !isnan(pm10) && !isnan(pm25)) {
+            Serial.print(F("PM10: "));
+            Serial.println(float(pm10) / 10);
+            ext_pm10 = pm10;
+            Serial.print(F("PM2.5: "));
+            Serial.println(float(pm25) / 10);
+            ext_pm25 = pm25;
+        }
+        Serial.println(F("End Handling SDS011 query data"));
+        });
+
+    if (!sds011.query_data_auto_async(pm_tablesize, pm25_table, pm10_table)) {
+        Serial.println(F("SDS011 measurement capture start failed"));
+    }
+
+    deadline = millis() + duty_s * 1000;
+    while (static_cast<int32_t>(deadline - millis()) > 0) {
+        delay(1000);
+        Serial.println(static_cast<int32_t>(deadline - millis()) / 1000);
+        sds011.perform_work();
+    }
 }
 
 
@@ -278,7 +363,7 @@ Adafruit_BME680 bme;
 void run_bme_sensor() {
 
   if (! bme.performReading()) {
-    Serial.println("Failed to perform reading :(");
+    Serial.println("Failed to perform reading of BME680 :(");
     return;
   }
   Serial.print("Temperature = ");
@@ -316,9 +401,8 @@ void setup ()
   Serial.begin(115200);
   while (!Serial);
   GPSserial.begin(9600, SERIAL_8N1, GPSTX, GPSRX);           //format is baud, mode, UART RX data, UART TX data 
-  SDSserial.begin(9600, SERIAL_8N1, SDSTX, SDSRX);           //format is baud, mode, UART RX data, UART TX data 
 
-  Wire1.begin(SDA, SCL);
+  Wire1.begin(SDA, SCL); // SDA = 21, SCL = 22
 
 
 SPI.begin(SCK,MISO,MOSI,SS);
@@ -343,6 +427,40 @@ SPI.begin(SCK,MISO,MOSI,SS);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
   bme.setGasHeater(320, 150); // 320*C for 150 ms
                               //
+/************* SDS011 ************************/
+    SDSserial.begin(9600, SERIAL_8N1, SDSRX, SDSTX); 
+    delay(100);
+
+    Serial.println(F("SDS011 start/stop and reporting sample"));
+
+    start_SDS();
+    Serial.print(F("SDS011 is running = "));
+    Serial.println(is_SDS_running);
+
+    String firmware_version;
+    uint16_t device_id;
+    if (!sds011.device_info(firmware_version, device_id)) {
+        Serial.println(F("Sds011::firmware_version() failed"));
+    }
+    else
+    {
+        Serial.print(F("Sds011 firmware version: "));
+        Serial.println(firmware_version);
+        Serial.print(F("Sds011 device id: "));
+        Serial.println(device_id, 16);
+    }
+
+    Sds011::Report_mode report_mode;
+    if (!sds011.get_data_reporting_mode(report_mode)) {
+        Serial.println(F("Sds011::get_data_reporting_mode() failed"));
+    }
+    if (Sds011::REPORT_ACTIVE != report_mode) {
+        Serial.println(F("Turning on Sds011::REPORT_ACTIVE reporting mode"));
+        if (!sds011.set_data_reporting_mode(Sds011::REPORT_ACTIVE)) {
+            Serial.println(F("Sds011::set_data_reporting_mode(Sds011::REPORT_ACTIVE) failed"));
+        }
+    }
+    /*************** END SDS011 **********************/
 }
 
 
@@ -367,12 +485,18 @@ void loop ()
 
         case DEVICE_STATE_SEND :
             {
-               // LoRaWAN.displaySending();
+              //  LoRaWAN.displaySending();
 
                 run_aq_sensor();
                 run_bme_sensor();
                 read_gps();
                 prepareTxFrame( appPort );
+                Serial.println("=== AQ ===");
+                Serial.print("pm 2.5: ");
+                Serial.println(ext_pm25);
+                Serial.print("pm 10: ");
+                Serial.println(ext_pm10);
+                Serial.println("=== END AQ ===");
                 LoRaWAN.send(loraWanClass);
                 deviceState = DEVICE_STATE_CYCLE;
             }
@@ -388,7 +512,7 @@ void loop ()
 
         case DEVICE_STATE_SLEEP :
             {
-                LoRaWAN.sleep (loraWanClass, debugLevel);
+               LoRaWAN.sleep (loraWanClass, debugLevel);
             }
             break;
 
